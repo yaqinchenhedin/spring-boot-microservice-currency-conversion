6cbdb6360c3d3d9e2523ae60bf4717df55d6fb4d
Yaqin Chen Hedin <Yaqin.Chen.Hedin@hiq.se>
2020-03-12 14:11:37 +0100

    first commit

---
+++ src/test/java/com/in28minutes/springboot/microservice/example/forex/springbootmicroserviceforexservice/SpringBootMicroserviceForexServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.in28minutes.springboot.microservice.example.forex.springbootmicroserviceforexservice;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootMicroserviceForexServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
